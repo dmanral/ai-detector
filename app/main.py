f6b9e17c14c766f6958f913180cc140087bf9787
Diyalo Manral <dmanral@dEvil.local>
2026-04-18 05:40:07 -0700

    Fix missing Request import in main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI, Depends, UploadFile, File
+from fastapi import FastAPI, Depends, UploadFile, File, Request
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
